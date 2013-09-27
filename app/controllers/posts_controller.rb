b2145e7acbc541b9b1c79491f8d65221bca99492
soothsayer <d.chernyatiev@gmail.com>
2013-09-27 15:26:42 +0000

    Base CRU

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,41 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(get_post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render "new"
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(get_post_params)
+      redirect_to @post
+    else
+      render "edit"
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+  def get_post_params
+    params.require(:post).permit(:title, :text)
+  end
+end
