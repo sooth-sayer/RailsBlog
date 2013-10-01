cbb53012665db7c296a733349a83ee25d49fd60a
soothsayer <d.chernyatiev@gmail.com>
2013-10-01 10:29:04 +0000

    test

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ module Blog
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.paths << "#{Rails}/vendor/assets/fonts"
-    config.assets.precompile << ".woff" << ".eot" << ".svg" << ".ttf"
+    config.assets.precompile += ["fonts/.woff", "fonts/.eot", "fonts/.svg", "fonts/.ttf"]
   end
 end
