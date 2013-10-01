1b333e506c6a157ebed11c844a15f903045103ee
soothsayer <d.chernyatiev@gmail.com>
2013-10-01 10:10:11 +0000

    Glyphicons file extension added to precompile list

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module Blog
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.paths << "#{Rails}/vendor/assets/fonts"
+    config.assets.precompile << ".woff" << ".eot" << ".svg" << ".ttf"
   end
 end
