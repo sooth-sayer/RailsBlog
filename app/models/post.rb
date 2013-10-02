847e2a879d5b32863cb21a42d0ea4e890c73b7ad
Alexey Kuznetsov <innercloister@gmail.com>
2013-10-02 13:12:15 +0000

    mount uploader to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 3 }
+  mount_uploader :picture, PictureUploader
 end
