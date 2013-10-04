8efa3e4fbfa92ea60e88acce03883aa48861483e
Dmitry Chernyatiev <d.chernyatiev@gmail.com>
2013-10-04 18:03:57 +0400

    Test env checking, notice if created

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,11 @@
 class CommentsController < ApplicationController
-  http_basic_authenticate_with name: "user", password: "user", only: :destroy
+  http_basic_authenticate_with name: "user", password: "user", only: :destroy unless Rails.env.test?
 
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment].permit(:commenter, :body))
+    flash[:notice] = "Comment was successfully created." if @comment
+
     redirect_to post_path(@post)
   end
 
