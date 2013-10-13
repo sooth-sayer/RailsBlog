c40bdccb426c2c91c11ebf0e11d8753d13aa091a
sooth-sayer <d.chernyatiev@gmail.com>
2013-10-14 00:14:55 +0400

    Email downcase

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ActiveRecord::Base
   validates :name, presence: true
   validates :email, uniqueness: true
   validates_email_format_of :email
+
+  before_save { self.email = email.downcase }
 end
