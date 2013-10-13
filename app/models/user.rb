c701506ee8c802cbfa228a432a236a813814353f
sooth-sayer <d.chernyatiev@gmail.com>
2013-10-14 00:32:56 +0400

    Email without case sensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,11 @@ class User < ActiveRecord::Base
   has_many :posts
 
   validates :name, presence: true
-  validates :email, uniqueness: true
+  validates :email, uniqueness: { case_sensitive: false }
   validates_email_format_of :email
+  validates :password, length: { minimum: 3 }
+
+  has_secure_password
 
   before_save { self.email = email.downcase }
 end
